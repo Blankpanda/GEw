5250afef2223e0e546eae42802a2387b7cd01710
Blankpanda <Blankpanda8@gmail.com>
2016-02-25 23:03:36 -0500

    added script to test api connection

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
 import exchange
-
+import json
+import time
+from urllib.request import urlopen
 
 def main():
-    test = exchange.Exchange(4151)
-    print(test.id)
-    print(test.name)
-    print(test.price)
-    print(test.description)
 
+    link = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="
+
+    f = open('res/items.json')
+    json_string = f.read()
+    item_dictionary = json.loads(json_string)
+
+    item_ids = []
+    counter = 0
+
+    for item in item_dictionary:
+        try:
+            print("Checking item ID: " + str(item['id']))
+            urlopen(link + str(item['id']))
+            counter = counter + 1
+            item_ids.append(item['id'])
+            time.sleep(5)
+        except Exception as e:
+            print(item['id']+ ": failed.")
+
+    w = open('results.txt')
+
+    for item in item_ids:
+        w.write(item + '\n')
+
+    print(counter)
+            
+            
 main()
